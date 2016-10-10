06de77801b4e72766c4782d8eb59b07db0f3ae5e
Kevin Zhang <kzhang@ivani.com>
2016-10-09 23:01:45 -0400

    debugging links

--- js/links.js
+++ js/links.js
@@ -6,83 +6,83 @@ $(document).ready(function() {
 //Jobs Links to Modals
   if(window.location.href.indexOf('#IvaniModal') != -1) {
     $('#IvaniModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
   }
   else if(window.location.href.indexOf('#ConnexinModal') != -1) {
     $('#ConnexinModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#BoxerModal') != -1) {
     $('#BoxerModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
 
 //Project Links to Modals
   else if(window.location.href.indexOf('#EdwinModal') != -1) {
     $('#EdwinModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#AquaponicsModal') != -1) {
     $('#AquaponicsModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#CapnChickenModal') != -1) {
     $('#CapnChickenModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#ISIMModal') != -1) {
     $('#ISIMModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#LiteModal') != -1) {
     $('#LiteModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#2048Modal') != -1) {
     $('#2048Modal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#GetRealModal') != -1) {
     $('#GetRealModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#REVOModal') != -1) {
     $('#REVOModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#DesnatModal') != -1) {
     $('#DesnatModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#BikesModal') != -1) {
     $('#BikesModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#MAOModal') != -1) {
     $('#MAOModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#IClubModal') != -1) {
     $('#IClubModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
   else if(window.location.href.indexOf('#DebateModal') != -1) {
     $('#DebateModal').modal('show');
-    window.location.hash=""
+    window.location.hash = "";
 
   }
 
